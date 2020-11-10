82793a01c4cd78a1cc98780799cce885b4edd0e2
Alexnorvag <norvag@mail.ru>
2020-11-10 22:53:35 +0300

    stack navigator: changes

--- src/app.js
+++ src/app.js
@@ -1,51 +1,44 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {getStatusBarHeight} from 'react-native-status-bar-height';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {HomeScreen, LoginScreen} from './screens';
 
 const Stack = createStackNavigator();
-// const Drawer = createDrawerNavigator();
-
-const MyTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: 'rgb(255, 45, 85)',
-  },
-};
 
 const App = () => {
   return (
-    <NavigationContainer theme={MyTheme}>
-      <Stack.Navigator
-        initialRouteName="Login"
-        // headerMode="screen"
-        screenOptions={{
-          title: 'UnMailMe',
-          headerLeft: null,
-          headerStyle: {
-            borderWidth: 0,
-            shadowColor: '#fff',
-            borderBottomColor: '#fff'
-          }
-        }}>
-        {/* <Stack.Navigator initialRouteName="Login" headerMode='screen'> */}
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Login"
+          headerMode="float"
+          screenOptions={{
+            title: 'UnMailMe',
+            headerLeft: null,
+            headerStyle: {
+              elevation: 0,
+              // borderWidth: 0,
+              // shadowColor: '#fff',
+              // borderBottomColor: '#fff',
+            },
+          }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
 const styles = StyleSheet.create({
   appContainer: {
-    backgroundColor: '#c0c',
-    paddingTop: getStatusBarHeight(),
+    // backgroundColor: '#c0c',
+    // paddingTop: getStatusBarHeight(),
   },
 });
 
