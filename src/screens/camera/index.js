26b69b85a252cc8e7ad892087992076a34b415b7
Alexey Gavron <norvag@mail.ru>
2020-11-28 23:39:19 +0300

    Update index.js

--- src/screens/camera/index.js
+++ src/screens/camera/index.js
@@ -9,7 +9,7 @@ export const CameraScreen = () => {
     if (cameraRef) {
       const options = {quality: 0.5, base64: true};
       const data = await cameraRef.takePictureAsync(options);
-      console.log(data.uri);
+      console.log('snap uri: ', data.uri);
     }
   };
 
